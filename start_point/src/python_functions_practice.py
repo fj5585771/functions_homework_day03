a7ae5b2f777e6c6dcb22377f990c165d6e0041f2
fraser jamieson <fraserjamieson.93@gmail.com>
2020-11-04 21:20:00 +0000

    2nd commit

--- start_point/src/python_functions_practice.py
+++ start_point/src/python_functions_practice.py
@@ -1,2 +1,11 @@
 def return_10():
     return 10
+
+def add(num__1, num_2):
+    num_3 = num__1 + num_2
+    return num_3
+
+#   def test_add(self):
+#       add_result = add( 1, 2 )
+#       self.assertEqual( 3, add_result )
+
